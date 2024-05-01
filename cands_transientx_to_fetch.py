3474766dd3cd4faddf9a4cdcacfd4ad18350f1d5
Joël van Ingen <j.c.ingen@gmail.com>
2024-05-01 17:44:43 +0200

    Add first steps of cands data conversion

---
+++ cands_transientx_to_fetch.py
@@ -0,0 +1,58 @@
+"""
+TransientX: Using data from https://github.com/ypmen/TransientX/blob/master/src/singlepulse/candplot.cpp, starting on line 415
+Fetch: Using data from readme.md or cand2h5 function in https://github.com/devanshkv/fetch/blob/master/bin/candmaker.py
+"""
+
+
+def parse_transientx_line(text: str):
+    """
+    TransientX cands file:
+    TAB separated
+    - s_ibeam
+    - candidate number
+    - timestamp (mjd)
+    - dm
+    - width
+    - snr
+    - freq stop
+    - freq start
+    - png filename
+    - s_id
+    - fil filename
+    """
+    data_list = text.split('\t')
+
+    return {
+        's_ibeam': data_list[0],
+        'candidate_number': data_list[1],
+        'timestamp': data_list[2],
+        'dm': data_list[3],
+        'width': data_list[4],
+        'snr': data_list[5],
+        'freq stop': data_list[6],
+        'freq start': data_list[7],
+        'png filename': data_list[8],
+        's_id': data_list[9],
+        'fil filename': data_list[10]
+    }
+
+
+def parse_transientx_file(transientx_filename: str):
+    with open(transientx_filename, 'r') as f:
+        return [parse_transientx_line(line) for line in f.readlines()]
+
+
+def write_fetch_file(fetch_filename: str, data: list[dict[str: str]]):
+    with open(fetch_filename, 'w') as f:
+        for line in data:
+            fetch_data = [
+                line['fil filename'],
+                line['snr'],
+                line['start time'],  # NON-EXISTENT
+                line['dm'],
+                line['width'],
+                line['label'],  # NON-EXISTENT
+                line['kill mask path']  # NON-EXISTENT
+            ]
+
+            f.write(','.join(fetch_data) + '\n')
