846068852be3bc412f307abf046de4ef50848716
Joel <j.c.ingen@gmail.com>
2024-05-23 00:26:34 +0200

    Fix another necessary type conversion

--- transientx_cands_converter.py
+++ transientx_cands_converter.py
@@ -81,7 +81,7 @@ def write_heimdall_file(heimdall_filename: str, data: list[CandidateData]) -> No
             heimdall_data = [
                 candidate.fil_filename,
                 candidate.snr,
-                candidate.tcand,
+                str(candidate.tcand),
                 str(log2(float(candidate.width))),
                 candidate.dm,
             ]
